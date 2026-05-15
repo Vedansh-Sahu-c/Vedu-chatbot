69fd7f869f86fb414623cbf397a65f1af349771b
Vedansh-Sahu-c <01vedansh2007@gmail.com>
2026-05-15 15:37:25 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -7,6 +7,8 @@ import google.generativeai as genai
 import faiss
 import numpy as np
 from pypdf import PdfReader
+import PyPDF2
+from io import BytesIO
 
 @dataclass
 class Chunk:
@@ -24,7 +26,10 @@ class RetrievalResult:
     query: str
 
 class RAGEngine:
-    def __init__(self, api_key: str = None):
+   File "/mount/src/vedu-chatbot/app.py", line 202
+              if doc_blocks:
+              ^
+SyntaxError: expected 'except' or 'finally' block def __init__(self, api_key: str = None):
         # Initialize Google Generative AI with the provided API key if passed
         if api_key:
             genai.configure(api_key=api_key)
